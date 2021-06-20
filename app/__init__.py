9680da00f7059527301cb79c6bfa58abc2e59b86
Roykuo <kuochuwon@gmail.com>
2021-06-20 17:06:00 +0800

    first commit

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,22 @@ from app.main.controller.account_group_controller import api as account_group_ns
 blueprint = Blueprint("api",
                       __name__,
                       url_prefix="/api/v1")
+# HINT 測試快速新增API
+blueprint2 = Blueprint("api2",
+                       __name__,
+                       url_prefix="/api/v2")
+
+api2 = Api(blueprint2,
+           title="Second Roy Traffic light system demo",
+           version="0.2.0",
+           description="Roy Traffic light system demo")
+
+api2.add_namespace(auth_ns,
+                   path="/auth")
+api2.add_namespace(device_ns,
+                   path="")
+jwt._set_error_handler_callbacks(api2)
+
 api = Api(blueprint,
           title="Roy Traffic light system demo",
           version="0.1.0",
--- manage.py
+++ manage.py
@@ -8,13 +8,14 @@ base_dir = os.path.abspath(os.path.dirname(__file__))
 dotenv_path = os.path.join(base_dir, ".env")
 load_dotenv(dotenv_path=dotenv_path)
 
-from app import blueprint  # noqa: E402
+from app import blueprint, blueprint2  # noqa: E402
 from app.main import create_app, db  # noqa: E402
 from app.main.database import seed_db  # noqa: E402
 from app.main.database import seed_sample  # noqa: E402
 
 app = create_app(os.getenv("FLASK_CONFIG") or "development")
 app.register_blueprint(blueprint)
+app.register_blueprint(blueprint2)
 
 app.app_context().push()
 manager = Manager(app)
